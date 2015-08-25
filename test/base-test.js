9e6b0936d808a24ac245fc74b069a42f2b19d26f
Matt Malone <matt.malone1@gmail.com>
2015-08-25 11:34:35 +0100

    initial commit

---
+++ test/base-test.js
@@ -0,0 +1,7 @@
+describe('Give it some context', function () {
+  describe('maybe aeee2 bit more context here', function () {
+    it('shosserrresuld run here few assertions', function () {
+      expect(true).toBe(true);
+    });
+  });
+});
